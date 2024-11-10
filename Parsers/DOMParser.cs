7dbc816306fd57237fe006519e5c6f7ee8d8dd85
YanOleks <yanytskyi.oleksandr@gmail.com>
2024-11-11 01:18:44 +0200

    .

--- Parsers/DOMParser.cs
+++ Parsers/DOMParser.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace XMLator.Parsers
 {
@@ -10,12 +11,71 @@ namespace XMLator.Parsers
     {
         public Dictionary<string, List<string>> GetAllAttributes(Stream xmlStream)
         {
-            throw new NotImplementedException();
+            XmlDocument doc = new XmlDocument();
+            doc.Load(xmlStream);
+
+            Dictionary<string, List<string>> attributes = new Dictionary<string, List<string>>();
+
+            // Проходимо по всіх елементах і збираємо атрибути
+            foreach (XmlNode node in doc.DocumentElement.GetElementsByTagName("*"))
+            {
+                if (node.Attributes != null)
+                {
+                    foreach (XmlAttribute attribute in node.Attributes)
+                    {
+                        string attributeName = attribute.Name;
+                        string attributeValue = attribute.Value;
+
+                        // Якщо атрибут ще не існує в словнику, додаємо його
+                        if (!attributes.ContainsKey(attributeName))
+                        {
+                            attributes[attributeName] = new List<string>();
+                        }
+
+                        // Додаємо значення атрибута, якщо його ще немає
+                        if (!attributes[attributeName].Contains(attributeValue))
+                        {
+                            attributes[attributeName].Add(attributeValue);
+                        }
+                    }
+                }
+            }
+
+            return attributes;
         }
 
         public List<string> Search(Stream xmlStream, Dictionary<string, string> keyword)
         {
-            throw new NotImplementedException();
+            XmlDocument doc = new XmlDocument();
+            doc.Load(xmlStream);
+
+            List<string> result = new List<string>();
+
+            // Проходимо по всіх елементах і фільтруємо на основі відповідності ключовим словам
+            foreach (XmlNode node in doc.DocumentElement.GetElementsByTagName("*"))
+            {
+                bool match = true;
+                if (node.Attributes != null)
+                {
+                    foreach (KeyValuePair<string, string> entry in keyword)
+                    {
+                        // Перевіряємо наявність атрибута та його значення
+                        if (node.Attributes[entry.Key]?.Value != entry.Value)
+                        {
+                            match = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (match)
+                {
+                    // Додаємо весь елемент у форматі XML
+                    result.Add(node.OuterXml);
+                }
+            }
+
+            return result;
         }
     }
 }
--- Parsers/LINQParser.cs
+++ Parsers/LINQParser.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace XMLator.Parsers
 {
@@ -10,12 +11,33 @@ namespace XMLator.Parsers
     {
         public Dictionary<string, List<string>> GetAllAttributes(Stream xmlStream)
         {
-            throw new NotImplementedException();
+            XDocument doc = XDocument.Load(xmlStream);
+
+            var attributes = doc.Descendants()
+                .SelectMany(element => element.Attributes())  // отримуємо всі атрибути з кожного елемента
+                .GroupBy(attr => attr.Name.LocalName)         // групуємо за назвою атрибута
+                .ToDictionary(
+                    group => group.Key,                       // назва атрибута
+                    group => group.Select(attr => attr.Value) // значення атрибутів
+                                   .Distinct()                // унікальні значення
+                                   .ToList()
+                );
+
+            return attributes;
         }
 
-        public List<string> Search(Stream xmlStream, Dictionary<string, string> keyword)
+        public List<string> Search(Stream xmlStream, Dictionary<string, string> keywords)
         {
-            throw new NotImplementedException();
+            XDocument doc = XDocument.Load(xmlStream);
+
+            var result = doc.Descendants()
+                            .Where(element => keywords.All(keyword =>
+                                element.Attribute(keyword.Key) != null &&
+                                element.Attribute(keyword.Key).Value == keyword.Value))
+                            .Select(element => element.ToString())
+                            .ToList();
+
+            return result;
         }
     }
 }
